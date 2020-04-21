f5979a148c6ed0a0ca2645c42b17c655dc2398a1
Pranit5895 <guptapranit9@gmail.com>
2020-04-21 16:26:22 -0700

    Class book created with create and show book functions

--- libraryapp.cpp
+++ libraryapp.cpp
@@ -5,4 +5,29 @@
 #include<process.h>
 #include<string.h>
 #include<iomanip.h>
+class book
+{
+char bno[6];
+char bname[50];
+char aname[20];
+public:
+void create_book()
+{
+cout<<"\n NEW BOOK ENTRY..\n";
+cout<<"\nEnter the book no. ";
+cin>>bno;
+cout<<"\n\nEnter the Name of the book ";
+gets(bname);
+cout<<"\n\nEnter the Author's name ";
+gets(aname);
+cout<<"\n\nBook Created.. ";
+}
+void show_book()
+{
+cout<<"\nBook no.:- "<<bno;
+cout<<"\nBook Name : ";
+puts(bname);
+cout<<"\nAuthor's name :-	";
+puts(aname);
+}
 
