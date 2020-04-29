d9003aed65d09a5a5e71381cbc26da604f2d9504
Pranit5895 <guptapranit9@gmail.com>
2020-04-28 17:14:34 -0700

    Book deposit function declared (11/13) completed

--- libraryapp.cpp
+++ libraryapp.cpp
@@ -317,4 +317,43 @@ getch();
 fp.close();
 fp1.close();
 }
+void book_deposit()  //Function to deposit book
+{
+char sn[6],bn[6];
+int found=0,flag=0,day,fine;
+clrscr();
+cout<<"\n\n BOOK DEPOSIT ... ";
+cout<<"\n\n\tEnter the student's admission no. ";
+cin>>sn;
+fp.open("student.dat",ios::in||ios::out);
+fp1.open("book.dat",ios::in||ios::out);
+while(fp.read((char*)&st,sizeof(student))&& found==0)
+{ if(strcmpi(st.retadmno(),sn)==0)
+{ found=1;
+if(st.rettoken()==1)
+{ while(fp1.read((char*)&bk,sizeof(book))&& flag==0)
+{ if(strcmpi(bk.retbno(),st.retstbno())==0)
+{ bk.show_book();
+flag=1;
+cout<<"\n\nBook deposited in no. of days";
+cin>>day;
+if(day>15)
+{ fine=(day-15)*1 ;
+cout<<"\n\nFine has to deposited Rs. "<<fine; }
+st.resettoken();
+int pos=-1*sizeof(st);
+fp.seekg(pos,ios::cur);
+fp.write((char*)&st,sizeof(student));
+cout<<"\n\n\t Book deposited successfully "; }
+}
+if(flag==0)
+cout<<" Book no. does not exist "; }
+else
+cout<<"No book is issued..please check!! "; }
+}
+if(found==0)cout<<"Student record not exist.. ";
+getch();
+fp.close();
+fp1.close();
+}
 
