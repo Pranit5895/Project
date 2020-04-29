4d7d6d81d3e1683866931fa0451a16706cb5117d
Pranit5895 <guptapranit9@gmail.com>
2020-04-28 17:13:34 -0700

    Book issue function declared (10/13) completed

--- libraryapp.cpp
+++ libraryapp.cpp
@@ -278,4 +278,43 @@ while(fp.read((char*)&bk,sizeof(book)))
 fp.close();
 getch();
 }
+void book_issue()  //Function to issue book
+{
+char sn[6],bn[6];
+int found=0,flag=0;
+clrscr();
+cout<<"\n\nBOOK ISSUE... ";
+cout<<"\n\nEnter The student's admission no. ";
+cin>>sn;
+fp.open("student.dat",ios::in||ios::out);
+fp1.open("book.dat",ios::in||ios::out);
+while(fp.read((char*)&st,sizeof(student))&& found==0)
+{ if(strcmpi(st.retadmno(),sn)==0)
+{ found=1;
+if(st.rettoken()==0)
+{ cout<<"\n\n\tenter the book no. :- ";
+cin>>bn;
+while(fp1.read((char*)&bk,sizeof(book))&& flag==0)
+{ if(strcmpi(bk.retbno(),bn)==0)
+{ bk.show_book();
+flag=1;
+st.addtoken();
+st.getstbno(bk.retbno());
+int pos=-1*sizeof(st);
+fp.seekg(pos,ios::cur);
+fp.write((char*)&st,sizeof(student));
+cout<<"\n\n\t Book issued successfully "; }
+}
+if(flag==0)
+cout<<"Book does not exist ";
+}
+else
+cout<<"You have not returned the last book "; }
+}
+if(found==0)
+cout<<"Student record not exist.. ";
+getch();
+fp.close();
+fp1.close();
+}
 
