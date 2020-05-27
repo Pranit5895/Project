2985fd717cfc8c4a307226db7439b9b82528ba2d
Pranit5895 <guptapranit9@gmail.com>
2020-05-27 02:25:14 -0700

    1 for game is over

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -85,3 +85,12 @@ int checkwin()
     else if (square[7] == square[8] && square[8] == square[9])
 
         return 1;
+     else if (square[1] == square[4] && square[4] == square[7])
+
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+
+        return 1;
