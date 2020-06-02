4ff49523cfa324bb037c697ed8ff9ace19748a0b
Pranit5895 <guptapranit9@gmail.com>
2020-06-02 01:45:30 -0700

    Class product started

--- Supermarket-Billing.cpp
+++ Supermarket-Billing.cpp
@@ -6,3 +6,13 @@
 #include<stdio.h>
 #include<process.h>
 #include<fstream.h>
+
+//***************************************************************
+    // CLASS USED IN PROJECT
+    //****************************************************************
+
+    class product
+    {
+        int pno;
+        char name[50];
+        float price, qty, tax, dis;
