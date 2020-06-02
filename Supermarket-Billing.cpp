078a37ddc9f42279e21af72628b5e830be3eba6b
Pranit5895 <guptapranit9@gmail.com>
2020-06-02 01:46:07 -0700

    Function to create a product declared

--- Supermarket-Billing.cpp
+++ Supermarket-Billing.cpp
@@ -16,3 +16,15 @@
         int pno;
         char name[50];
         float price, qty, tax, dis;
+	 public:
+            void create_product()
+            {
+                cout << "\nPlease Enter The Product No. of The Product ";
+                cin >> pno;
+                cout << "\n\nPlease Enter The Name of The Product ";
+                gets(name);
+                cout << "\nPlease Enter The Price of The Product ";
+                cin >> price;
+                cout << "\nPlease Enter The Discount (%) ";
+                cin >> dis;
+            }
