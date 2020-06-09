b2ae5ff60e236b0442a495eff3a95439927176a0
Pranit5895 <guptapranit9@gmail.com>
2020-06-09 02:10:44 -0700

    Create_account() function declared on class Account

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -19,3 +19,17 @@ public:
 	int retdeposit() const;	//function to return balance
 	char rettype() const;	//function to return type of account
 };
+void account::create_account()
+{
+	cout<<"\nEnter The account No. :";
+	cin>>acno;
+	cout<<"\n\nEnter The Name of The account Holder : ";
+	cin.ignore();
+	cin.getline(name,50);
+	cout<<"\nEnter Type of The account (C/S) : ";
+	cin>>type;
+	type=toupper(type);
+	cout<<"\nEnter The Initial amount(>=500 for Saving and >=1000 for current ) : ";
+	cin>>deposit;
+	cout<<"\n\n\nAccount Created..";
+}
