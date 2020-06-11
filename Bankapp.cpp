f1bdd46cc02dc79375026a48a87273816e50b1f5
Pranit5895 <guptapranit9@gmail.com>
2020-06-11 00:27:18 -0700

    modify() function declared of class Account

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -41,3 +41,16 @@ void account::show_account() const
 	cout<<"\nType of Account : "<<type;
 	cout<<"\nBalance amount : "<<deposit;
 }
+void account::modify()
+{
+	cout<<"\nAccount No. : "<<acno;
+	cout<<"\nModify Account Holder Name : ";
+	cin.ignore();
+	cin.getline(name,50);
+	cout<<"\nModify Type of Account : ";
+	cin>>type;
+	type=toupper(type);
+	cout<<"\nModify Balance amount : ";
+	cin>>deposit;
+}
+
