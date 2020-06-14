1ac7b9b69074057007d190f183a415e2ab3f70ae
Pranit5895 <guptapranit9@gmail.com>
2020-06-14 02:05:24 -0700

    Main() function started

--- Quiz.c
+++ Quiz.c
@@ -7,3 +7,9 @@ void show_record();
 void reset_score();
 void help();
 void edit_score(float,char[]);
+int main()
+{
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
