f79bc2147259273c7c4fa9aa9616daf0a806c4b7
Pranit5895 <guptapranit9@gmail.com>
2020-06-14 02:12:54 -0700

    Function to reset score

--- Quiz.c
+++ Quiz.c
@@ -440,3 +440,15 @@ void show_record()
 	 fclose(f);
 	 getch();
 }
+void reset_score()
+{
+    system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);
+}
