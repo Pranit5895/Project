7c010e56d7fc1108bee6a6d6ff4f3a21a5724ca6
Pranit5895 <guptapranit9@gmail.com>
2020-06-14 02:06:05 -0700

    Welcome screen designed

--- Quiz.c
+++ Quiz.c
@@ -13,3 +13,20 @@ int main()
      float score;
      char choice;
      char playername[20];
+     mainhome:
+     printf("\t\t\t  << QUIZ GAME >>\n");
+     printf("\n\t\t****************************************");
+
+     printf("\n\t\t\t      WELCOME\n ");
+     printf("\n\t\t\t        to\n ");
+     printf("\n\t\t\t    THE QUIZ GAME ");
+     printf("\n\t\t");
+     printf("\n\t\t****************************************");
+     printf("\n\t\t   BECOME RICH EARN UPTO 1 MILLION $ ;-) ");
+     printf("\n\t\t****************************************");
+     printf("\n\t\t > Press S to start the game");
+     printf("\n\t\t > Press V to view the highest score  ");
+     printf("\n\t\t > Press R to reset score");
+     printf("\n\t\t > press H for help            ");
+     printf("\n\t\t > press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
