b900bc92e5ac75e3b4aec5a04d5b8b29abf1152c
Pranit5895 <guptapranit9@gmail.com>
2020-06-14 02:13:50 -0700

    Function for any sort help

--- Quiz.c
+++ Quiz.c
@@ -452,3 +452,22 @@ void reset_score()
 	fprintf(f,"%s,%.2f",nm,sc);
     fclose(f);
 }
+void help()
+{
+    system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded $100,000.");
+    printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************BEST OF LUCK*********************************");
