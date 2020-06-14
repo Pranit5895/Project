bf1478d53da34b00ef1848a5898f70529d13fa0d
Pranit5895 <guptapranit9@gmail.com>
2020-06-14 02:12:21 -0700

    Function to display record

--- Quiz.c
+++ Quiz.c
@@ -426,3 +426,17 @@ int main()
         }
       }
      }
+void show_record()
+{
+
+	 char name[20];
+	 float scr=0;
+	 FILE *f;
+	 f=fopen("score.txt","r");
+	 fscanf(f,"%s%f",&name,&scr);
+	 printf("\n\n\t\t*************************************************************");
+	 printf("\n\n\t\t %s has secured the Highest Score %f",name,scr);
+	 printf("\n\n\t\t*************************************************************");
+	 fclose(f);
+	 getch();
+}
