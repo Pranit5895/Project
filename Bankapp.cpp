3831fbc44bd2b92ed0ad4a10dbd09d016be53782
Pranit5895 <guptapranit9@gmail.com>
2020-06-18 04:07:29 -0700

    Function declaration

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -78,4 +78,12 @@ char account::rettype() const
 	return type;
 }
 //Class account finished with function declaration
+////    	function declaration
+void write_account();	//function to write record in binary file
+void display_sp(int);	//function to display account details given by user
+void modify_account(int);	//function to modify record of file
+void delete_account(int);	//function to delete record of file
+void display_all();		//function to display all account details
+void deposit_withdraw(int, int); // function to desposit/withdraw amount for given account
+void intro();	//introductory screen function
 
