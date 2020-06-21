fefbcca80822e5609cf7b06a17b470040cdcf782
Pranit5895 <guptapranit9@gmail.com>
2020-06-21 00:34:17 -0700

    Switch case declared for function calling as per the main menu

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -103,4 +103,40 @@ int main()          //main funtion
 		cout<<"\n\n\t06. CLOSE AN ACCOUNT";
 		cout<<"\n\n\t07. MODIFY AN ACCOUNT";
 		cout<<"\n\n\t08. EXIT";
+		cout<<"\n\n\tSelect Your Option (1-8) ";
+		cin>>ch;
+		system("cls");
+		switch(ch)
+		{
+		case '1':
+			write_account();
+			break;
+		case '2':
+			cout<<"\n\n\tEnter The account No. : "; cin>>num;
+			deposit_withdraw(num, 1);
+			break;
+		case '3':
+			cout<<"\n\n\tEnter The account No. : "; cin>>num;
+			deposit_withdraw(num, 2);
+			break;
+		case '4':
+			cout<<"\n\n\tEnter The account No. : "; cin>>num;
+			display_sp(num);
+			break;
+		case '5':
+			display_all();
+			break;
+		case '6':
+			cout<<"\n\n\tEnter The account No. : "; cin>>num;
+			delete_account(num);
+			break;
+		 case '7':
+			cout<<"\n\n\tEnter The account No. : "; cin>>num;
+			modify_account(num);
+			break;
+		 case '8':
+			cout<<"\n\n\tThanks for using bank managemnt system";
+			break;
+		 default :cout<<"\a";
+		}
 
