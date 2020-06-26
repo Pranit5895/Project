f2dd726c15717239622163b128aff7627c8f58d0
Pranit5895 <guptapranit9@gmail.com>
2020-06-26 03:25:20 -0700

    Function to modify record of a file

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -178,4 +178,34 @@ void display_sp(int n)     //function to read a specific record of file
 	if(flag==false)
 		cout<<"\n\nAccount number does not exist";
 }
+void modify_account(int n)      //function to modify record of file
+{
+	bool found=false;
+	account ac;
+	fstream File;
+	File.open("account.dat",ios::binary|ios::in|ios::out);
+	if(!File)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		return;
+	}
+	while(!File.eof() && found==false)
+	{
+		File.read(reinterpret_cast<char *> (&ac), sizeof(account));
+		if(ac.retacno()==n)
+		{
+			ac.show_account();
+			cout<<"\n\nEnter The New Details of account"<<endl;
+			ac.modify();
+			int pos=(-1)*static_cast<int>(sizeof(account));
+			File.seekp(pos,ios::cur);
+			File.write(reinterpret_cast<char *> (&ac), sizeof(account));
+			cout<<"\n\n\t Record Updated";
+			found=true;
+		  }
+	}
+	File.close();
+	if(found==false)
+		cout<<"\n\n Record Not Found ";
+}
 
