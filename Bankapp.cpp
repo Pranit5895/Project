5ab1a59cbd688d0f1d34a3b66a80a5d081f02775
Pranit5895 <guptapranit9@gmail.com>
2020-06-27 01:09:18 -0700

    function to delete record of file

--- Bankapp.cpp
+++ Bankapp.cpp
@@ -208,4 +208,31 @@ void modify_account(int n)      //function to modify record of file
 	if(found==false)
 		cout<<"\n\n Record Not Found ";
 }
+void delete_account(int n)         //function to delete record of file 
+{
+	account ac;
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open("account.dat",ios::binary);
+	if(!inFile)
+	{
+		cout<<"File could not be open !! Press any Key...";
+		return;
+	}
+	outFile.open("Temp.dat",ios::binary);
+	inFile.seekg(0,ios::beg);
+	while(inFile.read(reinterpret_cast<char *> (&ac), sizeof(account)))
+	{
+		if(ac.retacno()!=n)
+		{
+			outFile.write(reinterpret_cast<char *> (&ac), sizeof(account));
+		}
+	}
+	inFile.close();
+	outFile.close();
+	remove("account.dat");
+	rename("Temp.dat","account.dat");
+	cout<<"\n\n\tRecord Deleted ..";
+}
+
 
