9ccf9c2dc4ec099ec079ec95a2dd3f28d773c139
Pranit5895 <guptapranit9@gmail.com>
2020-06-28 05:54:01 -0700

    Data members declared

--- Currency-Converter.java
+++ Currency-Converter.java
@@ -1,2 +1,9 @@
 import java.util.*;
 import java.text.DecimalFormat;
+
+public class CurrencyConverter {
+
+	public static void main(String[] args) {
+
+		double amount, dollar, pound, code, euro, yen, ringgit, rupee;
+
