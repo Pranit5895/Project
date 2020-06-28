3765f4e11d71d574029833d454d3e5e61eb6251f
Pranit5895 <guptapranit9@gmail.com>
2020-06-28 05:55:25 -0700

    Welcome screen for user and inout option

--- Currency-Converter.java
+++ Currency-Converter.java
@@ -11,3 +11,13 @@ public class CurrencyConverter {
 
 		Scanner sc = new Scanner(System.in);
 
+		System.out.println("hi, Welcome to the Currency Converter!");
+
+		System.out.println("which currency You want to Convert ? ");
+
+		System.out.println("1:Ruppe \t2:Dollar \t3:Pound \n4:Euro \t5:Yen \t6:Ringgit ");
+		code = sc.nextInt();
+
+		System.out.println("How much Money you want to convert ?");
+		amount = sc.nextFloat();
+
