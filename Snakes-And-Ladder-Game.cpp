ca906401d941d5c2900d6d6a06e03469ed08df4e
Pranit5895 <guptapranit9@gmail.com>
2021-01-13 04:41:17 -0800

    Main function started with Intro of game and board

--- Snakes-And-Ladder-Game.cpp
+++ Snakes-And-Ladder-Game.cpp
@@ -8,3 +8,13 @@ void draw_line(int n, char ch);
 void board();
 void gamescore(char name1[], char name2[], int p1, int p2);
 void play_dice(int & score);
+
+void main()
+{
+    int player1 = 0, player2 = 0, lastposition;
+    char player1name[80], player2name[80];
+    clrscr();
+    randomize();
+    draw_line(50, '=');
+    cout << "\n\n\n\n\t\tSNAKE LADDER GAME\n\n\n\n";
+    draw_line(50, '=');
