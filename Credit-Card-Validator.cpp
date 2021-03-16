42f1c76724ace43a0526bb0c1167a5fc54a371c6
Pranit5895 <guptapranit9@gmail.com>
2021-03-16 23:20:05 +0530

    Step 1 CODE

--- Credit-Card-Validator.cpp
+++ Credit-Card-Validator.cpp
@@ -38,3 +38,11 @@ int main() {
          // Step 1 is to double every second digit, starting from the right. If it
         // results in a two digit number, add both the digits to obtain a single
         // digit number. Finally, sum all the answers to obtain 'doubleEvenSum'. 
+
+        for (int i = len - 2; i >= 0; i = i - 2) {
+            int dbl = ((ccNumber[i] - 48) * 2);
+            if (dbl > 9) {
+                dbl = (dbl / 10) + (dbl % 10);
+            }
+            doubleEvenSum += dbl;
+        }
