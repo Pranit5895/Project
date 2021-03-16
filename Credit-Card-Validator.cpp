faee5f50de3a1865e1e29a8080ec97dea173cede
Pranit5895 <guptapranit9@gmail.com>
2021-03-16 23:15:29 +0530

    Function for Checking Valid Numeric Input

--- Credit-Card-Validator.cpp
+++ Credit-Card-Validator.cpp
@@ -3,3 +3,12 @@
 #include <string>
 
 using namespace std;
+
+bool isNumberString(const string& s) {
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+        if (s[i] < '0' || s[i] > '9')
+            return false;
+    }
+    return true;
+}
