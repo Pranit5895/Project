2d81ab541fc1a4943537ea1b69e6de79f06f0b27
Pranit5895 <guptapranit9@gmail.com>
2021-03-16 23:17:23 +0530

    WHile loop started for input of Credit Card Number

--- Credit-Card-Validator.cpp
+++ Credit-Card-Validator.cpp
@@ -12,3 +12,17 @@ bool isNumberString(const string& s) {
     }
     return true;
 }
+
+int main() {
+    string ccNumber;
+    
+    cout << "This program uses the Luhn Algorigthm to validate a CC number." << endl;
+    cout << "You can enter 'exit' anytime to quit." << endl;
+
+    while (true) {
+        
+        cout << "Please enter a CC number to validate: ";
+        cin >> ccNumber;
+        
+        if (ccNumber == "exit")
+            break;
