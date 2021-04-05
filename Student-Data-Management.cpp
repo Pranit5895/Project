d6731f78c9da52e990f22bede7cde9ee7bcc6627
Pranit5895 <guptapranit9@gmail.com>
2021-04-05 12:20:00 +0530

    Option 1

--- Student-Data-Management.cpp
+++ Student-Data-Management.cpp
@@ -31,5 +31,41 @@ int main()
              << "6. Exit\nEnter option: ";
         cin >> option;
 
+        if (option == 1) {
+            cout << "Enter the number of students: ";
+            cin >> n;
+ 
+            count_n = count_n + n;
+ 
+            for (int i = 0; i < n; i++) {
+                ofstream outfile;
+                outfile.open("Example.txt", ios::app);
+                // The entire data of a single student is
+                // stored line-by-line.
+                cout << "Enter your registration number: ";
+                cin >> data;
+                outfile << data << "\t";
+ 
+                cout << "Enter your name: ";
+                cin >> data;
+                int len = strlen(data);
+ 
+                while (len < 15) {
+                    data[len] = ' ';
+                    len = len + 1;
+                }
+                outfile << data << "\t";
+                // Inserting empty data initially into the
+                // file
+                outfile << empty << "\t";
+                outfile << empty << "\t";
+ 
+                cout << "Enter your proctor ID: ";
+                cin >> proctor;
+ 
+                outfile << proctor << endl;
+            }
+        }
+
 
         
