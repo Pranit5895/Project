5b2ee9c7c0cde2bf12c68697d205c85b9a9ffe3f
Pranit5895 <guptapranit9@gmail.com>
2021-04-05 12:16:28 +0530

    Variables declared

--- Student-Data-Management.cpp
+++ Student-Data-Management.cpp
@@ -2,3 +2,16 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+
+using namespace std;
+ 
+int main()
+{.
+    char data[15];
+    int n = 0, option = 0, count_n = 0;
+    // This is the initial mark alloted to a subject.
+    string empty = "00";
+    string proctor = "";
+    // Name of the file in which DB is stored.
+    ifstream f("Example.txt");
+    string line;
