161afaa976922348ebea9785560b3811c7fab9ea
Pranit5895 <guptapranit9@gmail.com>
2021-04-05 12:18:05 +0530

    While loop started for Operations

--- Student-Data-Management.cpp
+++ Student-Data-Management.cpp
@@ -21,3 +21,12 @@ int main()
     for (int i = 0; std::getline(f, line); ++i) {
         count_n++;
     }
+    while (option != 6) {
+        // This prints out all the available options in the
+        // DB
+        cout << "\nAvailable operations: \n1. Add New "
+                "Students\n2."
+             << "Student Login\n3. Faculty Login\n4. "
+                "Proctor Login\n5. Admin View\n"
+             << "6. Exit\nEnter option: ";
+        
