d550ca8b4a91ae41422b5dfc7c5651095f0dd853
Pranit5895 <guptapranit9@gmail.com>
2022-06-25 21:11:07 +0530

    Preparing Data

--- Predicting Crypto Prices in Python/main.py
+++ Predicting Crypto Prices in Python/main.py
@@ -16,4 +16,18 @@ end = dt.datetime.now()
 
 data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
 
-print(data.head())
+#Preparing Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaler_data = scaler.fit_transform(data['close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train, y_train = [] , []
+
+for x in range (prediction_days,len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1]),1)
+
