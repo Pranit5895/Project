6575b1d92410d00ba131949df1ee31c68752cba5
Pranit5895 <guptapranit9@gmail.com>
2022-06-28 20:35:28 +0530

    Model Testing

--- Predicting Crypto Prices in Python/main.py
+++ Predicting Crypto Prices in Python/main.py
@@ -1,6 +1,8 @@
 from asyncio import base_tasks
+from cProfile import label
 from calendar import EPOCH
 from pickletools import optimize
+from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -21,13 +23,13 @@ data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, e
 
 #Preparing Data
 scaler = MinMaxScaler(feature_range=(0,1))
-scaler_data = scaler.fit_transform(data['close'].values.reshape(-1,1))
+scaled_data = scaler.fit_transform(data['close'].values.reshape(-1,1))
 
 prediction_days = 60
 
 x_train, y_train = [] , []
 
-for x in range (prediction_days,len(scaled_data)):
+for x in range (prediction_days, len(scaled_data)):
     x_train.append(scaled_data[x-prediction_days:x, 0])
     y_train.append(scaled_data[x, 0])
 
@@ -44,7 +46,40 @@ model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
-model.Dense(units=1)
+model.add(Dense(units=1))
 
 model.compile(optimizer='Pranit', loss = 'mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+#Testing the Model
+
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'],test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.fit_transform(model_inputs)
+
+x_tests = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_tests.append(model_inputs[x-  prediction_days:x, 0])
+
+x_tests = np.array(x_tests)
+x_tests = np.reshape(x_tests, (x_tests.shape[0], x_tests.shape[1], 1))
+
+prediction_prices = model.predict( x_tests)
+prediction_prices = scaler.inverse_transform(prediction_prices)
+
+plt.plot(actual_prices, color='black', label='Actual prices')
+plt.plot(prediction_prices, color='green', label='Predicted Prices')
+plt.title(f'{crypto_currency} price prediction')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend(loc='upper left')
+plt.show()
